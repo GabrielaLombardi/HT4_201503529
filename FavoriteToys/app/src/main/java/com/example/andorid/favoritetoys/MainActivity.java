b5233498ce5bd296c93b7399c19f86779eae30a3
LombardiGaby <201503529@ingenieria.usac.edu.gt>
2017-07-03 20:36:25 +0200

    Ejercicio 2

--- FavoriteToys/app/src/main/java/com/example/andorid/favoritetoys/MainActivity.java
+++ FavoriteToys/app/src/main/java/com/example/andorid/favoritetoys/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.andorid.favoritetoys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.android.favoritetoys.ToyBox;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mToysListTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToysListTextView = (TextView) findViewById(R.id.tv_toys_name);
+
+        String[] toyNames = ToyBox.getToyNames();
+
+        for (String toyName : toyNames) {
+            mToysListTextView.append(toyName + "\n\n\n");
+        }
     }
 }
