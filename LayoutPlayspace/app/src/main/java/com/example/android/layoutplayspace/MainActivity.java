977e1cbd474d8248d26125a1c5c78fdf0560661c
LombardiGaby <201503529@ingenieria.usac.edu.gt>
2017-07-03 21:23:59 +0200

    Ejercicio 5

--- LayoutPlayspace/app/src/main/java/com/example/android/layoutplayspace/MainActivity.java
+++ LayoutPlayspace/app/src/main/java/com/example/android/layoutplayspace/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.layoutplayspace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+    }
+
+    // COMPLETED (8) Override onCreateOptionsMenu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // COMPLETED (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
+        getMenuInflater().inflate(R.menu.main, menu);
+        // COMPLETED (10) Return true to display your menu
+        return true;
     }
 }
